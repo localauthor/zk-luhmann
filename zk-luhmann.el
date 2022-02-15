62357b4248e9917078fa401e36ae8760b14a60cb
Grant Rosson <grantrosson@gmail.com>
2022-02-15 02:25:42 +0200

    Allow custom id prefix and postfix
    Based on work initiated by @jgru

--- zk-luhmann.el
+++ zk-luhmann.el
@@ -5,7 +5,7 @@
 ;; Author: Grant Rosson <https://github.com/localauthor>
 ;; Created: January 25, 2022
 ;; License: GPL-3.0-or-later
-;; Version: 0.2
+;; Version: 0.3
 ;; Homepage: https://github.com/localauthor/zk
 ;; Package-Requires: ((emacs "24.1")(zk "0.2"))
 
@@ -26,22 +26,48 @@
 
 ;; Adds support for files with Luhmann-style IDs in zk and zk-index.
 
-;; Luhmann-style IDs are alphanumeric sequences between curly braces.
-;; They look like this: {1,1,b,3 }
-;; Note the space after the final character. This is necessary for proper sorting.
+;; Luhmann-style IDs are alphanumeric sequences that immediately follow the
+;; zk-id in a note's filename. By default, the Luhmann-ID is surrounded by
+;; parentheses, with each character in the ID delimited by a comma. A note
+;; with such a Luhmann-ID will have a file name that looks something like:
 
-;; The ID is part of the file-name, positioned between the zk-id and the
-;; title.
+;;       "202012101215 (1,1,a,3,c) The origin of species.md"
 
 ;; Because all files with Luhmann-IDs have normal zk-ids, they are normal
-;; zk-files. This naming and ID scheme therefore simply offers a different
-;; organizing scheme within a zk. It is both fully integrated with zk while
-;; being, nevertheless, completely distinct --- a system within a system.
+;; zk-files. As a result, the naming and ID scheme supported by this package
+;; simply offers a different organizing scheme within a zk. It is both fully
+;; integrated with zk while being, nevertheless, completely distinct --- a
+;; system within a system.
 
 ;;; Code:
 
 (require 'zk)
 (require 'zk-index)
+
+;;; Variables
+
+(defgroup zk-luhmann nil
+  "Luhmann-ID support of zk."
+  :group 'text
+  :group 'files
+  :prefix "zk-luhmann")
+
+(defcustom zk-luhmann-id-prefix "("
+  "Character denoting the start of a Luhmann ID."
+  :type 'string)
+
+(defcustom zk-luhmann-id-postfix ")"
+  "Character denoting the end of a Luhmann ID."
+  :type 'string)
+
+(defcustom zk-luhmann-id-delimiter ","
+  "Character delimiting a Luhmann ID."
+  :type 'string)
+
+(defvar zk-luhmann-id-regex (concat zk-luhmann-id-prefix
+                                    "\\([0-9a-zA-Z,]*\\)"
+                                    zk-luhmann-id-postfix)
+  "Regexp to match Luhmann-IDs.")
 
 ;;; Luhmann ID Support
 
@@ -66,10 +92,10 @@
   (if transform
       (progn
         (string-match (concat "\\(?1:"
-                            zk-id-regexp
-                            "\\).\\(?2:.*?\\."
-                            zk-file-extension
-                            ".*\\)")
+                              zk-id-regexp
+                              "\\).\\(?2:.*?\\."
+                              zk-file-extension
+                              ".*\\)")
                       cand)
         (match-string 2 cand))
     "Luhmann Notes"))
@@ -79,10 +105,10 @@
   (sort list
         (lambda (a b)
           (let ((one
-                 (when (string-match "{\\([^ ]*\\)" a)
+		 (when (string-match zk-luhmann-id-regex a)
                    (match-string 1 a)))
                 (two
-                 (when (string-match "{\\([^ ]*\\)" b)
+		 (when (string-match zk-luhmann-id-regex b)
                    (match-string 1 b))))
             (string< one two)))))
 
@@ -92,7 +118,7 @@
         (pt (point)))
     (save-excursion
       (save-match-data
-        (when (re-search-backward "{" nil t)
+ 	(when (re-search-backward zk-luhmann-id-prefix nil t)
           (list (match-beginning 0)
                 pt
                 (zk-luhmann-format-candidates)
@@ -100,7 +126,7 @@
 
 (defun zk-luhmann-files ()
   "List notes with Luhmann-IDs."
-  (zk--directory-files t "{"))
+  (zk--directory-files t (concat zk-id-regexp " " zk-luhmann-id-prefix)))
 
 (defun zk-luhmann-format-candidates (&optional files)
   "Format completions candidates for FILES with Luhmann-IDs."
@@ -129,70 +155,84 @@
   "Focus on top level Luhmann-ID notes."
   (interactive)
   (let ((buffer-string (buffer-string)))
-    (zk-index (zk--directory-files t "{[^,] }")
-              zk-index-last-format-function
-              #'zk-luhmann-sort)
+    (zk-index (zk--directory-files
+               t
+               (concat zk-luhmann-id-prefix
+                       "[^"
+                       zk-luhmann-id-delimiter
+                       "]*"
+                       zk-luhmann-id-postfix))
+	      zk-index-last-format-function
+	      #'zk-luhmann-sort)
     (when (string= buffer-string (buffer-string))
       (zk-luhmann-index))))
 
 (defun zk-luhmann-index-forward ()
-  "Focus on this note and its immediate sub-branches."
   (interactive)
   (let* ((buffer-string (buffer-string))
-         (regexp "{.[^ }]*")
-         (line (buffer-substring
-                (line-beginning-position)
-                (line-end-position)))
-         (id (unless (string= "" line)
-               (unless (string-match regexp line)
+	 (regexp (concat zk-luhmann-id-prefix
+                         ".[^"
+                         zk-luhmann-id-postfix
+                         "]*" ))
+	 (line (buffer-substring
+		(line-beginning-position)
+		(line-end-position)))
+	 (id (unless (string= "" line)
+	       (unless (string-match regexp line)
                  (error "Not a Luhmann note"))
-               (match-string-no-properties 0 line)))
-         (str
-          (cond ((eq this-command 'zk-luhmann-index-forward)
-                 (concat id " \\|" id ",. [^ }]*"))
-                ((eq this-command 'zk-luhmann-index-unfold)
-                 (substring id 0 2)))))
+	       (match-string-no-properties 0 line)))
+	 (str
+	  (cond ((eq this-command 'zk-luhmann-index-forward)
+		 (concat
+                  id zk-luhmann-id-postfix "\\|"
+                  id zk-luhmann-id-delimiter "." zk-luhmann-id-postfix))
+		((eq this-command 'zk-luhmann-index-unfold)
+		 (substring id 0 2)))))
     (when id
       (progn
-        (zk-index (zk--directory-files t str)
-                  zk-index-last-format-function
-                  #'zk-luhmann-sort)
-        (goto-char (point-min))
-        (re-search-forward id nil t)
-        (beginning-of-line)
+	(zk-index (zk--directory-files t str)
+		  zk-index-last-format-function
+		  #'zk-luhmann-sort)
+	(goto-char (point-min))
+	(re-search-forward id nil t)
+	(beginning-of-line)
         (when (eq this-command 'zk-luhmann-index-unfold)
           (pulse-momentary-highlight-one-line nil 'highlight))))
     (cond ((and (eq this-command 'zk-luhmann-index-unfold)
-                (string= buffer-string (buffer-string)))
-           (zk-luhmann-index-top))
-          ((and (eq this-command 'zk-luhmann-index-forward)
-                (string= buffer-string (buffer-string)))
-           (progn
-             (setq this-command 'zk-luhmann-index-unfold)
-             (zk-luhmann-index-unfold))))))
+		(string= buffer-string (buffer-string)))
+	   (zk-luhmann-index-top))
+	  ((and (eq this-command 'zk-luhmann-index-forward)
+		(string= buffer-string (buffer-string)))
+	   (progn
+	     (setq this-command 'zk-luhmann-index-unfold)
+	     (zk-luhmann-index-unfold))))))
 
 (defun zk-luhmann-index-back ()
-  "Expand focus from current sub-branch scope."
   (interactive)
-  (if (re-search-forward "{" (line-end-position) t)
-      (zk-luhmann-index-sort)
-    (error "Not a Luhmann note"))
+  (zk-luhmann-index-sort)
   (let* ((buffer-string (buffer-string))
-         (line (buffer-substring (goto-char (point-min))
-                                 (line-end-position)))
-         (id (progn
-               (string-match "{.[^ ]*" line)
-               (match-string 0 line)))
-         (sub-id (substring (match-string 0 line) 0 -2)))
+	 (backward-rx (concat zk-luhmann-id-prefix
+                              ".[^"
+                              zk-luhmann-id-postfix
+                              "]*"))
+	 (line (buffer-substring (goto-char (point-min))
+				 (line-end-position)))
+	 (id (progn
+	       (string-match backward-rx line)
+	       (match-string 0 line)))
+	 (sub-id (substring (match-string 0 line) 0 -2)))
     (cond ((eq 2 (length id))
-            (zk-index (zk--directory-files t id)
-                      zk-index-last-format-function
-                      #'zk-luhmann-sort))
-          (t (progn (zk-index (zk--directory-files
+	   (zk-index (zk--directory-files t id)
+		     zk-index-last-format-function
+		     #'zk-luhmann-sort))
+	  (t (progn (zk-index (zk--directory-files
                                t
-                               (concat sub-id " \\|" sub-id ",. [^ }]*"))
-                       zk-index-last-format-function
-                       #'zk-luhmann-sort)
+                               (concat sub-id zk-luhmann-id-postfix
+                                       "\\|"
+                                       sub-id zk-luhmann-id-delimiter
+                                       "." zk-luhmann-id-postfix))
+		              zk-index-last-format-function
+		              #'zk-luhmann-sort)
                     (re-search-forward id nil t)
                     (beginning-of-line)
                     (pulse-momentary-highlight-one-line nil 'highlight))))
@@ -212,7 +252,7 @@
                    last-command-event
                  (get last-command-event 'ascii-character)))
          (reps (- (- (logand char ?\177) ?0) 1))
-         (base-rx "{[0-9]*")
+         (base-rx (concat zk-luhmann-id-prefix "[0-9]*"))
          (slug ",.")
          (new-slug "")
          (regexp
@@ -220,7 +260,7 @@
             (when reps
               (dotimes (_ reps)
                 (setq new-slug (concat new-slug slug))))
-            (concat base-rx new-slug " ")))
+            (concat base-rx new-slug zk-luhmann-id-postfix)))
          (current-files (zk--parse-id 'file-path (zk-index--current-id-list)))
          (files (remq nil
                       (mapcar
@@ -237,7 +277,7 @@
   (interactive)
   "Open ZK-Index buffer and to line of current note."
   (let ((id (zk--current-id)))
-    (zk-index (zk--directory-files "{")
+    (zk-index (zk-luhmann-files)
               zk-index-last-format-function
               #'zk-luhmann-sort)
     (other-window 1)
